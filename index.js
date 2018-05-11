c70366a56a7964f7a9b506c406cbe5ad75822451
Sitthi <kamnan43@gmail.com>
2018-05-11 23:32:45 +0700

    png

--- index.js
+++ index.js
@@ -287,8 +287,8 @@ function saveImage(set) {
 			});
 			var form = req.form();
 			form.append('file', body, {
-				filename: (index + 1) + '.jpg',
-				contentType: 'image/jpeg'
+				filename: (index + 1) + '.png',
+				contentType: 'image/png'
 			});
 			form.append('set', set);
 		});
